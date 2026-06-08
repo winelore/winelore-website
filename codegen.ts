abdce83eb517939f4187c59d655b9af3ea5f417f
oryshych <denys.oryshych@gmail.com>
2026-06-08 15:33:17 +0300

    Added GraphQL Codegen configuration and scripts for code generation

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+    schema: 'http://switchback.proxy.rlwy.net:43233/graphql',
+    documents: ['src/**/*.{ts,tsx}'],
+    generates: {
+        './src/gql/': {
+            preset: 'client',
+            plugins: [],
+            presetConfig: {
+                gqlTagName: 'gql',
+            }
+        }
+    },
+    ignoreNoDocuments: true,
+};
+
+export default config;
