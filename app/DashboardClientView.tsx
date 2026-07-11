7ed177e4d5377d67533d5878b8d5a2591d84e704
Viktor <mr.grory@gmail.com>
2026-07-11 16:57:20 +0300

    gap-3

--- app/DashboardClientView.tsx
+++ app/DashboardClientView.tsx
@@ -230,7 +230,7 @@ export default function WineLoreDashboard({
             )}
 
       {/* Competition Cards Grid */}
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-start flex-1">
+        <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-start flex-1">
           {initialCompetitions.map((competition) => (
             <CompetitionCard
               key={competition.id}
